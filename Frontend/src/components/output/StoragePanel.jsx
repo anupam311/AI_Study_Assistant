60a5bddb87e6ffd2dd6457a16304f75e8b973046
Anupam311 <chaudharyanupam746@gmail.com>
2026-04-04 23:14:24 +0530

    Feat: add copy-to-clipboard functionalty for output panels.

--- Frontend/src/components/output/StoragePanel.jsx
+++ Frontend/src/components/output/StoragePanel.jsx
@@ -1,14 +1,31 @@
 import { marked } from "marked";
+import { useState } from "react";
 
 function StoragePanel({ value }){
+
+    const [copied, setCopied] = useState(false);
     
     const htmlContent = marked.parse(value || "");
 
+    function handleCopy() {
+        if (!value) return;
+
+        navigator.clipboard.writeText(value);
+        setCopied(true);
+
+        setTimeout(() => setCopied(false), 1500);
+    }
     return(
-        <div
-            className="panels" 
-            dangerouslySetInnerHTML={{ __html: htmlContent}} 
+        <div className="panel-wrapper">
+            <button className="copy-btn" onClick={handleCopy}>
+                {copied ? "Copied!" : "Copy"}
+            </button>
+
+            <div
+                className="panels" 
+                dangerouslySetInnerHTML={{ __html: htmlContent}} 
             />
+        </div>
     );
 }
 
